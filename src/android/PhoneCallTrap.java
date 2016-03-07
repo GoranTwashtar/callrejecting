ad5b29d4be56ff4a70e2d9c31c310ec559096636
Goran Lukich <info@twashtar.com>
2016-03-07 22:38:18 +0200

    Fix

--- src/android/PhoneCallTrap.java
+++ src/android/PhoneCallTrap.java
@@ -58,10 +58,6 @@ class CallStateListener extends PhoneStateListener {
 
             case TelephonyManager.CALL_STATE_RINGING:
             msg = "RINGING";
-//            Intent i = new Intent(Intent.ACTION_MEDIA_BUTTON);
-//	    i.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(KeyEvent.ACTION_UP,
-//            KeyEvent.KEYCODE_HEADSETHOOK));
-//	    context.sendOrderedBroadcast(i, null);
             ignoreCall();
             exitCleanly();
             break;
@@ -72,45 +68,5 @@ class CallStateListener extends PhoneStateListener {
 
         callbackContext.sendPluginResult(result);
     }
-}
-
-private void ignoreCall() {
-    if (USE_ITELEPHONY)
-        ignoreCallAidl();
-    else
-        ignoreCallPackageRestart();
-}
-/**
- * AIDL/ITelephony technique for ignoring calls
- */
-private void ignoreCallAidl() {
-    try {
-        // telephonyService.silenceRinger();
-
-        telephonyService.endCall();
-    } catch (RemoteException e) {
-        e.printStackTrace();
-        Log.d(tag, "ignoreCall: " + "Error: " + e.getMessage());
-
-    } catch (Exception e) {
-        e.printStackTrace();
-        Log.d(tag, "ignoreCall" + "Error: " + e.getMessage());
-
-    }
-}
-/**
- * package restart technique for ignoring calls
- */
-private void ignoreCallPackageRestart() {
-    ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-    am.restartPackage("com.android.providers.telephony");
-    am.restartPackage("com.android.phone");
-}
-/**
- * cleanup and exit routine
- */
-private void exitCleanly() {
-    unHookReceiver();
-    this.finish();
-
+//}
 }
